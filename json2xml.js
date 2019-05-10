0ff70b1c7b213a620b6a8d56d828ef4ebb34cdc2
Kamczii <45385166+Kamczii@users.noreply.github.com>
2019-05-10 21:35:13 +0200

    Update json2xml.js

--- json2xml.js
+++ json2xml.js
@@ -1,5 +1,7 @@
 
 
+//Przykładowy json z tłumaczeniem
+//Tłumaczenie nazwy obiektu pod zmienną "name"
 var translations = {
     "name": "Imie",
     "number": "Wiek",
@@ -23,6 +25,7 @@ var translations = {
     
 };
 
+//Przykładowy json
 var json = {  
    "name":"<imie>Imie testowe</imie></Imie>",
    "number":"31",
@@ -40,21 +43,24 @@ var json = {
 };
 
 
-xml = generateXmlFromJson(json, translations);
+xml = generateXmlFromJson(json, translations);      //zastosowanie funkcji
 
-document.getElementById("result").innerHTML=xml;
+document.getElementById("result").innerHTML=xml;    
 
+
+//Funkcja przyjmuje 2 jsony, jeden z zawartością do konwersji, drugi z tłumaczeniem
 
 function generateXmlFromJson(json, translations){
-    translationsPoint = translations;
-    var finalXML = "<root>";
-    result = createXml(json);
+    
+    translationsPoint = translations;   //zmienna, która służy nam do przetrzymania aktualnej pozycji w jsonie z tłumaczeniami (im głębiej w rekurencji, tym dalej w głąb jsona)
+    var finalXML = "<root>"; //tutaj zaczynamy definiować zmienną, którą finalnie zwrócimy, zaczynamy od znacznika root.
+    result = createXml(json);   //Wywołujemy funkcję pomocniczą do generowania jsona
     finalXML+="</root>";
     
     console.log(finalXML);
     return finalXML;
 
-
+//Funkcja pomocnicza wykorzystująca rekurencję, section to odpowiadająca nazwa przetłumaczonego obiektu np. Adres
     function createXml(json, section){
         result = "";
         word = "";
@@ -68,16 +74,14 @@ function generateXmlFromJson(json, translations){
         for(var i=0;i<jsonVariables.length;i++){
             
             variable = json[jsonVariables[i]];
-            if(typeof variable === 'object' && variable !== null){
-                word = translationsPoint[jsonVariables[i]].name;
-                translationsPoint = translationsPoint[jsonVariables[i]];
+            if(typeof variable === 'object' && variable !== null){ //Tutaj wchodzimy w obiekt jsona np. address
+                word = translationsPoint[jsonVariables[i]].name;            //tłumaczenie
+                translationsPoint = translationsPoint[jsonVariables[i]];    //ustawiamy akutalny punkt w tłumaczeniu
                 variable = json[jsonVariables[i]];
-                console.log(variable);
                 result = createXml(variable, word);
-            } else {
-                word = translationsPoint[jsonVariables[i]];
+            } else {                                                //Tutaj wchodzimy w zmienne obiektów jsona np. city
+                word = translationsPoint[jsonVariables[i]];         //Tłumaczenie, nie ustawiamy zmiennej translationsPoint ponieważ nie wchodzimy w głąb innego obiektu
                 variable = json[jsonVariables[i]];
-                console.log(variable);
                 variable = checkForSpecialChars(variable.toString());
                 finalXML+= "<"+word+">"+variable+"</"+word+">";
             }
@@ -90,6 +94,7 @@ function generateXmlFromJson(json, translations){
         return result;
     }
     
+    //Funkcja pomocnicza która zamienia znaki xml na odpowiadające im kody html
     function checkForSpecialChars(string){
         string = string.replace(/<+/g,"&lt");
         string = string.replace(/>+/g,"&gt");
